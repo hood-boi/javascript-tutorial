824fccd2cbb29be512164de5dbf71770790db970
hood-boi <salman.adams@mail.utoronto.ca>
2019-04-15 20:44:14 -0400

    t1

---
+++ TUT_001-Variables/script.js
@@ -0,0 +1,13 @@
+const print = console.log;
+
+//Declare variables
+var a;
+print('a : ', a); //Expected : Undefined
+
+//Define variables
+a = 42;
+print('a : ', a); //Expected : 42
+
+//Declare and Define variables all at once
+var b = 42;
+print('b : ', b); //Expected : 42
